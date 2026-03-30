f95b6b1c2b61c8f5b594579fdc071365e5d19c2e
Safayet <mdsafayet315@gmail.com>
2026-03-30 06:48:50 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navbar from './component/Navbar'
+import './index.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
